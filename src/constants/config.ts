4e37030f8e4f204591a04a8f6302fbcaa5372541
Faisal Tariq <faisaltariq183@gmail.com>
2024-02-16 17:44:28 -0800

    update_Url

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: 'WorldRanks: Unveiling Global Standings',
   description: `Explore WorldRanks – your ultimate source for country rankings. Discover a concise snapshot of nations' standings in various categories, from economic performance to social indicators. Uncover valuable insights and comparisons effortlessly. Stay informed with WorldRanks – where countries take their place on the global stage.`,
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://world-ranks-mu-three.vercel.app',
 };
